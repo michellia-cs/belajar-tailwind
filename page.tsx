6a7a13c7e936e974d7942aed839cfb30223ef796
michellia-cs <118815277+michellia-cs@users.noreply.github.com>
2023-05-04 20:46:01 +0700

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -12,6 +12,10 @@ export default function Home() {
     <h1 className="text-lightyellow text-4xl border-4">
     Discover Your Needs!
   </h1>
+  <h2 className="text-xl note">Title 2</h2>
+  <p>A regular paragraph</p>
+  <p className="text-sm note">A description paragraph</p>
+  <p className="text-xs note">A little note</p>
   </div>
   )
 }
